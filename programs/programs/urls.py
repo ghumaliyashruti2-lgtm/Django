54dd047637b4a19a3a23c00349aca4fd1bc76854
ghumaliyashruti2-lgtm <ghumaliyashruti2@gmail.com>
2025-11-12 11:13:14 +0530

    import views and create url aboutus and courses

--- programs/programs/urls.py
+++ programs/programs/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_programs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('aboutus/',views.aboutus),
+    path('courses/',views.courses),
 ]
