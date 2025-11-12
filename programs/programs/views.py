99c7a19ba7dd41b9bc57aca6cea48e78f7748303
ghumaliyashruti2-lgtm <ghumaliyashruti2@gmail.com>
2025-11-12 11:58:03 +0530

    create a modul homepage

--- programs/programs/views.py
+++ programs/programs/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request,"index.html")
 
 def aboutus(request):
     return HttpResponse("welcome to shruti")
