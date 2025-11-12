a99d82cdd8a3da9f1de13d5fc1f86b141b03f308
ghumaliyashruti2-lgtm <ghumaliyashruti2@gmail.com>
2025-11-12 11:33:15 +0530

    file modified with courseid

--- programs/programs/views.py
+++ programs/programs/views.py
@@ -6,3 +6,5 @@ def aboutus(request):
 def courses(request):
     return HttpResponse("welcome to courses")
 
+def course_details(request,course_id):
+    return HttpResponse("welcome to " + str(course_id))
