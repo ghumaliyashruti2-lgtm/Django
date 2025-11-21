1bc50d234cb320ea6cec93a10b1f33104dcd86cf
ghumaliyashruti2-lgtm <ghumaliyashruti2@gmail.com>
2025-11-21 16:30:32 +0530

    import path for smtp and add one if condition for media debug at last

--- programs/programs/urls.py
+++ programs/programs/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from programs import views
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import send_mail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,3 +39,6 @@ urlpatterns = [
     path('marksheet/',views.marksheet),
     path('newsdetails/<new_slug>',views.newsdetails)
     ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
