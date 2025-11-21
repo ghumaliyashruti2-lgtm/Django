3b7534e5b109858e9c951b91ba51b5d556063f79
ghumaliyashruti2-lgtm <ghumaliyashruti2@gmail.com>
2025-11-21 16:27:16 +0530

    add media and smtp command at last in settings.py file

--- programs/programs/settings.py
+++ programs/programs/settings.py
@@ -128,3 +128,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR,"static"]
+
+MEDIA_ROOT = BASE_DIR /"media"
+
+MEDIA_URL = "/media/"
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_HOST_USER='shrutighumaliyaofficial@gmail.com'
+EMAIL_HOST_PASSWORD='ltaj mrmg bfcw inxy'
+EMAIL_USE_TLS=True
