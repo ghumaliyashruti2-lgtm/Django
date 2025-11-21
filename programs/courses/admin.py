2e799c8dabc3177c22efb1c529cb9d5ff02cb40f
ghumaliyashruti2-lgtm <ghumaliyashruti2@gmail.com>
2025-11-21 16:28:59 +0530

    add image field and makemigration to migrate in courses_data

--- programs/courses/admin.py
+++ programs/courses/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from courses.models import Courses
 # Register your models here.
 class CoursesAdmin(admin.ModelAdmin):
-    list_display = ('courses_title','courses_description')
+    list_display = ('courses_image','courses_title','courses_description')
 admin.site.register(Courses,CoursesAdmin)
 
--- programs/courses/models.py
+++ programs/courses/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Courses(models.Model):
     courses_title=models.CharField(max_length=50)
     courses_description=models.TextField()
+    courses_image =models.FileField(upload_to="courses/",max_length=250,null=True,default=None)
 # Create your models here.
